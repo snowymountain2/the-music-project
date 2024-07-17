3b3839e3761d5044dc6943e8e6d5fbbf2e64829b
john-paul <johnpaulcody8@gmail.com>
2024-07-17 17:39:09 -0400

    scaffolding main content area on homepage

--- artist-snapshot/src/Homepage/HomepageSection.tsx
+++ artist-snapshot/src/Homepage/HomepageSection.tsx
@@ -20,20 +20,31 @@ export function HomepageSection() {
       <div className="main-container">
         <div className="top-songs">
           <p>
-            ply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to
-            make a type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typese
+            <h2>Top Songs</h2>
+            <ol>
+              <li>asdfsafasdfsf</li>
+              <li>asdfsafasdfsf</li>
+              <li>asdfsafasdfsf</li>
+              <li>asdfsafasdfsf</li>
+              <li>asdfsafasdfsf</li>
+              <li>asdfsafasdfsf</li>
+              <li>asdfsafasdfsf</li>
+              <li>asdfsafasdfsf</li>
+              <li>asdfsafasdfsf</li>
+              <li>asdfsafasdfsf</li>
+            </ol>
           </p>
         </div>
         <div className="top-videos">
           <p>
-            ply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to
-            make a type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typese
+            <h2> Top Videos </h2>
+            <div className="top-videos-items">
+              <div className="placeholder"></div>
+              <div className="placeholder"></div>
+              <div className="placeholder"></div>
+              <div className="placeholder"></div>
+              <div className="placeholder"></div>
+            </div>
           </p>
         </div>
         <div className="trending-topics">
