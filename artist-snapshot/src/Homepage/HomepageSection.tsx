7d588fc1ea3921c5e3cacbbfd8037635afbe9ee6
john-paul <johnpaulcody8@gmail.com>
2024-07-17 17:23:00 -0400

    added elements to header

--- artist-snapshot/src/Homepage/HomepageSection.tsx
+++ artist-snapshot/src/Homepage/HomepageSection.tsx
@@ -2,13 +2,20 @@ export function HomepageSection() {
   return (
     <>
       <div className="header">
-        <p>
-          ply dummy text of the printing and typesetting industry. Lorem Ipsum
-          has been the industry's standard dummy text ever since the 1500s, when
-          an unknown printer took a galley of type and scrambled it to make a
-          type specimen book. It has survived not only five centuries, but also
-          the leap into electronic typese
-        </p>
+        <div className="logo">TheArtistSnapshot</div>
+        <div className="search-div">
+          <form action="" method="POST" className="form">
+            <label htmlFor="artist-search"></label>
+            <input
+              type="search"
+              className="search-field"
+              name="search-field"
+              value="Search Artist"
+              id="artist-search"
+            ></input>
+            <input type="submit" value="Submit"></input>
+          </form>
+        </div>
       </div>
       <div className="main-container">
         <div className="top-songs">
