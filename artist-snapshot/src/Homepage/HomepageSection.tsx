f869c4939f4e58851e572f06c8bae2464cc6a71a
john-paul <johnpaulcody8@gmail.com>
2024-07-18 17:11:25 -0400

    added all html elements and basical positioning of elements

--- artist-snapshot/src/Homepage/HomepageSection.tsx
+++ artist-snapshot/src/Homepage/HomepageSection.tsx
@@ -44,26 +44,37 @@ export function HomepageSection() {
               <div className="placeholder"></div>
               <div className="placeholder"></div>
               <div className="placeholder"></div>
+              <div className="placeholder"></div>
             </div>
           </p>
         </div>
-        <div className="trending-topics">
-          <p>
-            ply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to
-            make a type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typese
-          </p>
-        </div>
-        <div className="top-albums">
-          <p>
-            ply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to
-            make a type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typese
-          </p>
+        <div className="container-three">
+          <div className="trending-topics">
+            <h2>Trending Music Topics</h2>
+            <p>
+              <ul>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+                <li>sdfasdffddsfddfsddfsfdfdssdf</li>
+              </ul>
+            </p>
+          </div>
+          <div className="top-albums">
+            <h2>Trending Albums</h2>
+            <div className="album-container">
+              <div className="albums"></div>
+              <div className="albums"></div>
+              <div className="albums"></div>
+              <div className="albums"></div>
+            </div>
+          </div>
         </div>
       </div>
     </>
