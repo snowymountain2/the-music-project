af9a3600c64c726b9015676bd935041e574c8af8
john-paul <johnpaulcody8@gmail.com>
2024-08-15 19:47:44 -0400

    deleted some unnecessary code in server.js file

--- artist-snapshot/server/server.js
+++ artist-snapshot/server/server.js
@@ -9,84 +9,6 @@ const app = express();
 
 app.use(cors());
 
-// async function retrievePopularTopics() {
-//   puppeteer.use(StealthPlugin());
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.setDefaultNavigationTimeout(0);
-//   await page.goto("https://www.billboard.com/c/music/music-news/");
-
-//   const popularTopicsData = await page.evaluate(() =>
-//     Array.from(document.querySelectorAll("h3"), (e) => e.innerText)
-//   );
-//   await browser.close();
-//   return popularTopicsData;
-// }
-
-// async function retrievePopularSongs() {
-//   puppeteer.use(StealthPlugin());
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.setDefaultNavigationTimeout(0);
-//   await page.goto("https://acharts.co/us_singles_top_100");
-
-//   const popularSongsData = await page.evaluate(() =>
-//     Array.from(
-//       document.querySelectorAll("td.cPrinciple > a > span"),
-//       (e) => e.innerText
-//     )
-//   );
-//   await browser.close();
-//   return popularSongsData;
-// }
-
-// async function retrievePopularAlbums() {
-//   puppeteer.use(StealthPlugin());
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.setDefaultNavigationTimeout(0);
-//   await page.goto("https://acharts.co/us_albums_top_100");
-
-//   const popularAlbumsData = await page.evaluate(() =>
-//     Array.from(document.querySelectorAll(".cImg img"), (e) => e.src)
-//   );
-//   await browser.close();
-//   return popularAlbumsData;
-// }
-
-// async function retrievePopularVideos() {
-//   puppeteer.use(StealthPlugin());
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.setDefaultNavigationTimeout(0);
-//   await page.goto("https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl");
-
-//   const popularVideoData = await page.evaluate(() =>
-//     Array.from(document.querySelectorAll("h3"), (e) => e.innerHTML)
-//   );
-
-//   // extracts only youtube videos that contain music video in them from trending list of vids that contain a wide assortment of stuff
-//   const listFilteredToMusicVideos = popularVideoData.filter((e) =>
-//     e.includes("Music Video")
-//   );
-
-//   // extracts the youtube vid id from string of each videos html
-//   const getMusicVideoYouTubeIds = listFilteredToMusicVideos.map((e) => {
-//     const youtubeVideoIds = e.slice(
-//       e.indexOf("?v=") + 3,
-//       e.indexOf(">", e.indexOf("?v=")) - 1
-//     );
-//     return youtubeVideoIds;
-//   });
-
-//   const result = await db.query("INSERT INTO webdata (test) VALUEs($1)", [
-//     getMusicVideoYouTubeIds,
-//   ]);
-
-//   await browser.close();
-//   return getMusicVideoYouTubeIds;
-// }
-
 async function retrieveConcertData(cityTrimmed, stateTrimmed) {
   puppeteer.use(StealthPlugin());
   const browser = await puppeteer.launch({
@@ -140,20 +62,6 @@ async function retrieveConcertData(cityTrimmed, stateTrimmed) {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.post("/api", async (req, res) => {
-  const { desc } = req.body;
-  console.log(desc);
-  // const additionToDataBase = await db.query(
-  //   "INSERT INTO webdata (ex) VALUE($1)",
-  //   [desc]
-  // );
-  const result = await db.query("INSERT INTO webdata (test) VALUEs($1)", [
-    ["deddddddsc"],
-  ]);
-  // console.log(result.rows[0]);
-  // res.send(result.rows[0]);
-});
 
 app.post("/location", async (req, res) => {
   const formDataFromClient = req.body;
