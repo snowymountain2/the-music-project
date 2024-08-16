6cc0b732220b51c83b69e57bbcd6116b220a0ad3
john-paul <johnpaulcody8@gmail.com>
2024-08-16 15:38:19 -0400

    added environment variables

--- artist-snapshot/server/db.js
+++ artist-snapshot/server/db.js
@@ -1,26 +1,15 @@
-// import Pool from "pg";
+import "dotenv/config";
 import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: "john-paul",
-  password: "Singyy90!@",
-  host: "localhost",
-  port: 5432,
-  database: "webdatadb",
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  database: process.env.DATABASE,
 });
 
 export const query = (text, params, callback) => {
   return pool.query(text, params, callback);
 };
-
-// import pg from "pg";
-// const { Client } = pg;
-
-// export const config = new Client({
-//   user: "john-paul",
-//   password: "Singyy90!@",
-//   host: "localhost",
-//   port: 5432,
-//   database: "webdatadb",
-// });
